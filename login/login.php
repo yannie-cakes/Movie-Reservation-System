18ce8f68f740baf18af78ee7c62ae045d76f50c6
yannie-cakes <170949044+yannie-cakes@users.noreply.github.com>
2024-12-12 12:38:39 +0800

    login w logout php

--- login/login.php
+++ login/login.php
@@ -1,24 +1,32 @@
 <?php
-include('../database_connection.php'); // For more easier connection
+session_start();
+include ('../database_connection.php');
 
 $errorMessage = null;
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
     $USERNAME = $_POST['user-input'];
     $PASSWORD = $_POST['pass-input'];
 
-    $sql="SELECT EMP_ROLE from EMPLOYEE WHERE EMP_LNAME='$USERNAME' AND EMP_ID='$PASSWORD'";
+    $sql = "SELECT EMP_ID, EMP_FNAME, EMP_LNAME, EMP_ROLE FROM EMPLOYEE WHERE EMP_LNAME = '$USERNAME' AND EMP_ID = '$PASSWORD'";
     $result = $mysqli->query($sql);
 
-    if (strlen($PASSWORD) !== 8 || !ctype_digit($PASSWORD) ) {
+    if (strlen($PASSWORD) !== 8 || !ctype_digit($PASSWORD)) {
         $errorMessage = 'Username and/or password is incorrect. Please try again.';
-    } else if ($result->num_rows > 0) {
+    } elseif ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        if($row['EMP_ROLE'] === 'Admin')
-            header('Location: ../admin/emp_database.html');
-        else if($row['EMP_ROLE'] === 'Employee')
-            header('Location: ./transaction/transaction.html');   
+
+        $_SESSION['EMP_FNAME'] = $row['EMP_FNAME'];
+        $_SESSION['EMP_LNAME'] = $row['EMP_LNAME'];        
+
+        if ($row['EMP_ROLE'] === 'Admin') {
+            header('Location: ../admin/emp_database/emp_database.php');
+            exit();
+        } else if ($row['EMP_ROLE'] === 'Employee') {
+            header('Location: ../employee/movies/movie_database.php');
+            exit();
+        }
     } else {
         $errorMessage = 'Username and/or password is incorrect. Please try again.';
     }
